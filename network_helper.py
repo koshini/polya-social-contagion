4b0ce1a60adfbd7277efa7b562fe53dfc13c44e0
Connor Killackey <connorkillackey@gmail.com>
2019-03-05 01:39:37 -0500

    Added regression as a curing strategy and looked into entropy

--- network_helper.py
+++ network_helper.py
@@ -1,14 +1,16 @@
 import networkx as nx
 import random
+import numpy as np
 import copy
 import operator as op
 import math
+from regression import RegressionModel
 
 
 class NetWorkHelper():
     def __init__(self, initial_condition):
         self.node_count = initial_condition['node_count']
-        self.edges = initial_condition['edges']
+        self.parameter = initial_condition['parameter']
         self.dist = initial_condition['dist']
         self.red = initial_condition['red']
         self.black = initial_condition['black']
@@ -19,15 +21,15 @@ class NetWorkHelper():
         self.red_strat = initial_condition['red_strat']
         self.black_strat = initial_condition['black_strat']
         self.type = initial_condition['type']
-        self.heuristic_param = initial_condition['heuristic_param']
         self.G = None
+        self.regression_model = RegressionModel()
 
     # TODO: Add distribution of red/black balls in network toggle
     def create_network(self):
         # Generate a barabasi albert graph with node_count nodes
         # Setting the second parameter to 1 means each node added will only have one edge to begin
         if self.type == 'barabasi':
-            G = nx.barabasi_albert_graph(self.node_count, self.edges)
+            G = nx.barabasi_albert_graph(self.node_count, self.parameter)
         elif self.type == 'path':
             G = nx.path_graph(self.node_count)
         # Initializes urn dictionary
@@ -55,44 +57,47 @@ class NetWorkHelper():
         nx.set_node_attributes(G, name="prev_exposure", values=prev_exposure) # not being used
         nx.set_node_attributes(G, name="prev_deltar", values=0)
         nx.set_node_attributes(G, name="prev_deltab", values=0)
+        nx.set_node_attributes(G, name="entropy", values=[])
         
         #Set the intitial budget distributions
         G.node[0]['prev_deltar'] = self.red_budget
         G.node[0]['prev_deltab'] = self.black_budget
+                
+        self.black_dist = self.equally_divide(self.black_budget)
+        self.red_dist = self.equally_divide(self.red_budget)
         
         #Set initial exposure rate
         self.G = G
         self.set_prev_exposure()
+        self.set_centrality_mult()
         return self.G
 
     def run_time_step(self):
         if self.black_strat == 'uniform':
-            self.black_dist = self.equally_divide(self.black_budget)
+            curing_dist = self.equally_divide(self.black_budget)
         if self.black_strat == 'random':
-            self.black_dist = self.constrained_sample_sum_pos(self.black_budget)            
+            curing_dist = self.constrained_sample_sum_pos(self.black_budget)            
         if self.black_strat == 'gradient':
-            self.black_gradient_descent()
-        if self.black_strat == 'heuristic':
-            self.black_heuristic_strat()
-
-
-        
-        # print('Black dist:', self.black_dist)
+            curing_dist = self.black_gradient_descent()
+        if self.black_strat == 'centrality':
+            curing_dist = self.black_centrality_ratio_strat()
+        if self.black_strat == 'regression':
+            curing_dist = self.run_regression()
+        #print('Black dist:', self.black_dist)
 
         if self.red_strat == 'uniform':
-            self.red_dist = self.equally_divide(self.red_budget)
+            infecting_dist = self.equally_divide(self.red_budget)
         if self.red_strat == 'random':
-            self.red_dist = self.constrained_sample_sum_pos(self.red_budget)
+            infecting_dist = self.constrained_sample_sum_pos(self.red_budget)
         if self.red_strat == 'gradient':
-            self.red_gradient_descent()
-        if self.red_strat == 'heuristic':
-            self.red_heuristic_strat()
+            infecting_dist = self.red_gradient_descent()
+        if self.red_strat == 'centrality':
+            infecting_dist = self.red_centrality_ratio_strat()
+            
+        self.set_distributions(curing_dist, infecting_dist)
+        #print('Red dist:', self.red_dist)
 
-        # print('Red dist:', self.red_dist)
-
-        
-
-        
+            
     def equally_divide(self, total):
         if self.node_count <= 0:
             return []
@@ -135,9 +140,15 @@ class NetWorkHelper():
         # network_infection(Si,n) = proportion of the red balls in the node's super urn
         network_infection = super_urn['red'] / (super_urn['red'] + super_urn['black'])
         super_urn['network_infection'] = network_infection
+        node[1]['network_infection'] = network_infection
         node[1]['super_urn'] = super_urn
         return super_urn
     
+    def record_entropy(self):
+        for index, node in self.G.node.items():
+            p = node['network_infection']
+            node['entropy'].append(-p*math.log(p)-(1-p)*math.log(1-p))
+        
     def find_superurn_exp_red(self, infecting_strat = None):
         if infecting_strat == None:
             infecting_strat = self.red_dist
@@ -145,10 +156,10 @@ class NetWorkHelper():
         exp_red = {}
         for node in self.G.node.items():
             exp_red_temp = 0 
-            exp_red_temp += node[1]['super_urn']['network_infection']*self.red_dist[node[0]]
+            exp_red_temp += node[1]['super_urn']['network_infection']*infecting_strat[node[0]]
             neighbors = nx.all_neighbors(self.G, node[0])
             for nd in neighbors:
-                exp_red_temp += self.G.node[nd]['super_urn']['network_infection']*self.red_dist[nd]
+                exp_red_temp += self.G.node[nd]['super_urn']['network_infection']*infecting_strat[nd]
             exp_red[node[0]] = exp_red_temp
         return exp_red
             
@@ -167,7 +178,7 @@ class NetWorkHelper():
         return exp_black
     
 
-    def calc_node_partial_exposure(self, node, exp_red, exp_black):
+    def calc_node_partial_exposure_black(self, node, exp_red, exp_black):
         node_exp = node[1]['super_urn']['network_infection']
         neighbors = nx.all_neighbors(self.G, node[0])
         all_nodes = []
@@ -181,17 +192,28 @@ class NetWorkHelper():
             partial_exp_sum += (numerator/denominator)
         return partial_exp_sum
         
+    def calc_node_partial_exposure_red(self, node, exp_red, exp_black):
+        node_exp = node[1]['super_urn']['network_infection']
+        neighbors = nx.all_neighbors(self.G, node[0])
+        all_nodes = []
+        all_nodes.append(node[0])
+        partial_exp_sum = 0
+        for x in neighbors:
+            all_nodes.append(x)
+        for nd in all_nodes:
+            numerator = -(self.G.node[nd]['super_urn']['black'] + exp_black[nd])*(node_exp)
+            denominator = (self.G.node[nd]['super_urn']['black'] + self.G.node[nd]['super_urn']['red'] + exp_red[nd] + exp_black[nd])**2
+            partial_exp_sum += (numerator/denominator)
+        return partial_exp_sum
+    
     #Also doesnt always output full budget worth of distribution
     def black_gradient_descent(self):
         #This stays constant at every step of the gradient descent right now
         #due to us only changing our curing strategy
         exp_red = self.find_superurn_exp_red()
         step = 0.1
-        curing_strat = self.node_count * [0]
-        infecting_strat = self.node_count * [0]
-
-        curing_strat[0] = self.black_budget
-        infecting_strat[0] = self.red_budget
+        curing_dist = self.node_count * [0]
+        curing_dist[0] = self.black_budget
         
         for k in range(0,100):
             next_partial_exposures = []
@@ -199,28 +221,25 @@ class NetWorkHelper():
             for node in self.G.node.items():
                 next_strat.append(0)
                 
-            exp_black = self.find_superurn_exp_black(curing_strat)
+            exp_black = self.find_superurn_exp_black(curing_dist)
             for node in self.G.node.items():
-                next_partial_exposures.append(self.calc_node_partial_exposure(node, exp_red, exp_black))
+                next_partial_exposures.append(self.calc_node_partial_exposure_black(node, exp_red, exp_black))
             min_index = next_partial_exposures.index(min(next_partial_exposures))
             next_strat[min_index] = self.black_budget
-            temp_array = list( map(op.sub, next_strat, curing_strat) )
+            temp_array = list( map(op.sub, next_strat, curing_dist) )
             temp_array = [x*step for x in temp_array]
-            curing_strat = list( map(op.add, curing_strat, temp_array) )
-        for index,x in enumerate(curing_strat):
-            curing_strat[index] = round(x)
-        self.black_dist = curing_strat
+            curing_dist = list( map(op.add, curing_dist, temp_array) )
+        for index,x in enumerate(curing_dist):
+            curing_dist[index] = round(x)
+        return curing_dist
     
     def red_gradient_descent(self):
         #This stays constant at every step of the gradient descent right now
         #due to us only changing our curing strategy
         exp_black = self.find_superurn_exp_black()
         step = 0.1
-        curing_strat = self.node_count * [0]
-        infecting_strat = self.node_count * [0]
-
-        curing_strat[0] = self.black_budget
-        infecting_strat[0] = self.red_budget
+        infecting_dist = self.node_count * [0]
+        infecting_dist[0] = self.red_budget
         
         for k in range(0,100):
             next_partial_exposures = []
@@ -228,62 +247,69 @@ class NetWorkHelper():
             for node in self.G.node.items():
                 next_strat.append(0)
                 
-            exp_red = self.find_superurn_exp_black(infecting_strat)
+            exp_red = self.find_superurn_exp_red(infecting_dist)
             for node in self.G.node.items():
-                next_partial_exposures.append(self.calc_node_partial_exposure(node, exp_red, exp_black))
+                next_partial_exposures.append(self.calc_node_partial_exposure_red(node, exp_red, exp_black))
             min_index = next_partial_exposures.index(min(next_partial_exposures))
             next_strat[min_index] = self.red_budget
-            temp_array = list( map(op.sub, next_strat, infecting_strat) )
+            temp_array = list( map(op.sub, next_strat, infecting_dist) )
             temp_array = [x*step for x in temp_array]
-            infecting_strat = list( map(op.add, infecting_strat, temp_array) )
-        for index,x in enumerate(infecting_strat):
-            infecting_strat[index] = round(x)
-        self.red_dist = infecting_strat
+            infecting_dist = list( map(op.add, infecting_dist, temp_array) )
+        for index,x in enumerate(infecting_dist):
+            infecting_dist[index] = round(x)
+        return infecting_dist
 
-    def get_heuristic_parameters(self):
+    def get_centrality_infection(self):
+        # closeness centrality
         closeness_centrality_dict = nx.closeness_centrality(self.G)
-        parameter_sum = 0
 
+        centrality_infection_sum = 0
         for index, node in self.G.node.items():
-            if 'degree' in self.heuristic_param:
-                degree = self.G.degree(index)
-            else:
-                degree = 1
-
-            if 'centrality' in self.heuristic_param:
-                centrality = closeness_centrality_dict[index]
-            else:
-                centrality = 1
-
-            if 'infection' in self.heuristic_param:
-                infection = node['super_urn']['network_infection']
-
-            else:
-                infection = 1
+            degree = self.G.degree(index)
+            closeness_centrality = closeness_centrality_dict[index]
+            infection_rate = node['super_urn']['network_infection']
             ##### parameters: degree, closeness, infection
-            heuristic_parameters = degree*centrality*infection
-            node['heuristic_parameters'] = heuristic_parameters
-            parameter_sum += heuristic_parameters
-        return parameter_sum
+            centrality_infection = degree*closeness_centrality*infection_rate
+            node['centrality_infection'] = centrality_infection
+            centrality_infection_sum += centrality_infection
+        return centrality_infection_sum
 
     #TODO: Fix this: This does not always output a distribution that adds to budget.
     #We should probably also run the entire strat with one call and not give it node by node
-    def black_heuristic_strat(self):
-        for node in self.G.node.items():
-            parameter_sum = self.get_heuristic_parameters()
-            ratio = node[1]['heuristic_parameters'] / parameter_sum
-            balls = round(self.black_budget * ratio)
-            self.black_dist[node[0]] = balls
-        return balls
+    def black_centrality_ratio_strat(self):
+        infection_array = np.array(list(nx.get_node_attributes(self.G,'network_infection').values()))
+        centrality_mult_array = np.array(list(nx.get_node_attributes(self.G,'centrality_multiplier').values()))
+        curing_dist = np.multiply(infection_array, centrality_mult_array)
+        curing_dist = curing_dist / sum(curing_dist)
+        curing_dist = np.around(curing_dist * self.black_budget)        
+        return list(curing_dist)
 
-    def red_heuristic_strat(self):
-        for node in self.G.node.items():
-            parameter_sum = self.get_heuristic_parameters()
-            ratio = node[1]['heuristic_parameters'] / parameter_sum
-            balls = round(self.red_budget * ratio)
-            self.red_dist[node[0]] = balls
-        return balls
-
+    def red_centrality_ratio_strat(self):
+        infection_array = np.array(list(nx.get_node_attributes(self.G,'network_infection').values()))
+        centrality_mult_array = np.array(list(nx.get_node_attributes(self.G,'centrality_multiplier').values()))
+        infecting_dist = np.multiply(infection_array, centrality_mult_array)
+        infecting_dist = infecting_dist / sum(infecting_dist)
+        infecting_dist = np.around(infecting_dist * self.red_budget)        
+        return list(infecting_dist)
+    
+    def set_centrality_mult(self):
+        closeness_centrality_dict = nx.closeness_centrality(self.G)
+        for index, node in self.G.node.items():
+            degree = self.G.degree(index)
+            closeness_centrality = closeness_centrality_dict[index]
+            centrality_mult = degree*closeness_centrality
+            node['centrality_multiplier'] = centrality_mult
+        
+    def set_distributions(self, curing_dist, infecting_dist):
+        self.black_dist = curing_dist
+        self.red_dist = infecting_dist
+        
+    def run_regression(self):
+        infection_array = []
+        for index, node in self.G.node.items():
+            infection_array.append(node['super_urn']['network_infection'])
+        curing_dist = list(self.regression_model.output_dist(infection_array, self.black_budget))
+        return curing_dist
 
         
         
